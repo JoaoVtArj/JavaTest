d98a480a22e6fdd44d4c392da7c3229df7f9084b
JoaoVtArj <joaovitorpda22@gmail.com>
2023-08-30 15:02:20 -0300

    testecond

---
+++ TesteCondicio.java
@@ -0,0 +1,69 @@
+package exercicio1;
+
+import java.util.Scanner;
+
+public class TesteCondicio {
+
+	/*public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int horaInicial = sc.nextInt();
+		int horaFinal = sc.nextInt();
+		int duracao;
+
+		if (horaInicial < horaFinal) {
+			duracao = horaFinal - horaInicial;
+		} else {
+			duracao = 24 - horaInicial + horaFinal;
+			System.out.println("O JOGO DUROU " + duracao + " HORAS");
+		}
+		sc.close();
+	}*/
+	
+	/*public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int codigo = sc.nextInt();
+        int quantidade = sc.nextInt();
+        double total;
+        
+        if (codigo == 1) {
+        	total = quantidade * 4.0;
+        }
+        else if (codigo == 2) {
+            total = quantidade * 4.5;
+        }
+        else if (codigo == 3) {
+            total = quantidade * 5.0;
+        }
+        else if (codigo == 4) {
+            total = quantidade * 2.0;
+        }
+        else {
+        	total = quantidade * 1.5;
+        }
+        System.out.printf("Total: R$ %.2f%n", total);
+        sc.close();
+	}
+	
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	double intervalos = sc.nextDouble();
+	if ( intervalos < 0.0 || intervalos > 100.00) {
+		System.out.println("Intervalo inválido");
+	}
+	else if (intervalos <= 25.0) {
+		System.out.printf("Intervalo: [0,25]");
+	}
+	else if (intervalos <= 50.0) {
+        System.out.printf("Intervalo: [25,50]");
+    }
+	else if (intervalos <= 75.0) {
+        System.out.printf("Intervalo: [50,75]");
+    } else {
+    	System.out.printf("Intervalo: [75,100]");
+    }
+	
+	sc.close();	
+	}*/
+	
+	
+}
